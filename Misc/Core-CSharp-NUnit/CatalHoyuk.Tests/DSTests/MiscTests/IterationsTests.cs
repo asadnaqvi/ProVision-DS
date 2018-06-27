3b64672fe21c9cb9d6d1aa26030328db8c96c710
asad.naqvi <asad.naqvi12@gmail.com>
2018-06-27 18:42:08 +0500

    Biggest Binary gap between two 1s
    Biggest Binary gap between two 1s

---
+++ Misc/Core-CSharp-NUnit/CatalHoyuk.Tests/DSTests/MiscTests/IterationsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CatalHoyuk.DS.Misc;
+using NUnit.Framework;
+
+namespace CatalHoyuk.Tests.DSTests.MiscTests
+{
+    [TestFixture]
+    public class IterationsTests
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void GetBiggestBinaryGap_PositiveNumber_0(int input)
+        {
+            int result = new Iterations().GetBiggestBinaryGap(input);
+            Assert.That(result, Is.EqualTo(0));
+        }
+       
+        [Test]
+        public void GetBiggestBinaryGap_Five_1()
+        {
+            int result = new Iterations().GetBiggestBinaryGap(5);
+            Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void GetBiggestBinaryGap_Nine_2()
+        {
+            int result = new Iterations().GetBiggestBinaryGap(9);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void GetBiggestBinaryGap_MinusOne_0()
+        {
+            int result = new Iterations().GetBiggestBinaryGap(-1);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
