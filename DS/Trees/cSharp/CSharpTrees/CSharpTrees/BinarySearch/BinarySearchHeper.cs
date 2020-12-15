8cf663e7bfa98a9fabf40638c7e872dee0fab51c
CDOCS\asad.naqvi <asad.naqvi@wolterskluwer.com>
2020-12-16 02:32:25 +0500

    adding comments

--- DS/Trees/cSharp/CSharpTrees/CSharpTrees/BinarySearch/BinarySearchHeper.cs
+++ DS/Trees/cSharp/CSharpTrees/CSharpTrees/BinarySearch/BinarySearchHeper.cs
@@ -6,20 +6,32 @@ namespace CSharpTrees.BinarySearch
 {
     public class BinarySearchHeper
     {
+        /// <summary>
+        /// 1- max<min means we traversed the list and item not found
+        /// 2- Find the mid point of the list
+        /// 3- If key is greater than mid point then search for the right half of the list
+        /// 4- If key is less than mid point then search for the left half of the list
+        /// 5- if key equals to mid point then return mid point
+        /// </summary>
+        /// <param name="key">item that we need to find</param>
+        /// <param name="array"> list of items. We are using int array for this example</param>
+        /// <param name="min">minimum index</param>
+        /// <param name="max">maximum index</param>
+        /// <returns></returns>
         public static int BinarySearch(int key, int[] array, int min, int max)
         {
             if (max < min)
             {
                 return -1;
             }
-            var midPoint = FindMidPoint(min, max);
+            var midPoint = FindMidPoint(array[min], array[max]);
             if (array[midPoint] < key) { return BinarySearch(key, array, midPoint + 1, max); }
             else if (array[midPoint] > key) { return BinarySearch(key, array, min, midPoint - 1); }
             else return midPoint;
         }
         private static int FindMidPoint(int min, int max)
         {
-            return (min + max) / 2;
+            return (min + max)/ 2;
         }
     }
 }
